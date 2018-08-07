1cbc0ed542e4287bac5c5d919278a31a731f76ac
Ashton Charbonneau <ashton@ashtonc.ca>
2018-08-07 15:11:05 -0600

    Add TA, blog, and debate to deploy script
    Allows me to sync static sites for my teaching assistant site, my blog,
    and my debate site. Still a WIP.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,15 @@
+# Base home directory
 gsutil -m rsync -r -x ".git/" "ashtonc" "gs://ashtonc.ca"
+
+# TA content
+cd ta; hugo; cd ..
+gsutil -m rsync -r -x ".git/" "ta/public" "gs://ashtonc.ca/ta"
+
+# Debate content
+#cd debate; hugo; cd ..
+#gsutil -m rsync -r -x ".git/" "debate/public" "gs://ashtonc.ca/debate"
+
+# Blog content
+#cd blog; hugo; cd ..
+#gsutil -m rsync -r -x ".git/" "blog/public" "gs://ashtonc.ca/blog"
+
