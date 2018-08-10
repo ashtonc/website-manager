0bc569a0ccc15752ddcd6857b4d79125be45a33b
Ashton Charbonneau <ashton@ashtonc.ca>
2018-08-09 20:49:36 -0600

    Update destination bucket
    Had to do this due to CNAME interference with MX records.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 # Base home directory
-gsutil -m rsync -r -x ".git/" "ashtonc" "gs://ashtonc.ca"
+gsutil -m rsync -r -x ".git/" "ashtonc" "gs://www.ashtonc.ca"
 
 # TA content
 cd ta; hugo; cd ..
-gsutil -m rsync -r -x ".git/" "ta/public" "gs://ashtonc.ca/ta"
+gsutil -m rsync -r -x ".git/" "ta/public" "gs://www.ashtonc.ca/ta"
 
 # Debate content
 cd debate; hugo; cd ..
-gsutil -m rsync -r -x ".git/" "debate/public" "gs://ashtonc.ca/debate"
+gsutil -m rsync -r -x ".git/" "debate/public" "gs://www.ashtonc.ca/debate"
 
 # Blog content
 #cd blog; hugo; cd ..
-#gsutil -m rsync -r -x ".git/" "blog/public" "gs://ashtonc.ca/blog"
+#gsutil -m rsync -r -x ".git/" "blog/public" "gs://www.ashtonc.ca/blog"
 
