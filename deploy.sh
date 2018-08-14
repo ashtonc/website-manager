bb23befd173964e5ae22a82ab7eda496c7dc34ec
Ashton Charbonneau <ashton@ashtonc.ca>
2018-08-13 18:09:48 -0600

    Add google app engine deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,49 @@
-# Base home directory
-gsutil -m rsync -r -x ".git/" "ashtonc" "gs://www.ashtonc.ca"
+# Variables
 
-# TA content
-cd ta; hugo; cd ..
-gsutil -m rsync -r -x ".git/" "ta/public" "gs://www.ashtonc.ca/ta"
+home_bucket="www.ashtonc.ca"
+home_app_engine="ashtonc-home-app"
 
-# Debate content
-cd debate; hugo; cd ..
-gsutil -m rsync -r -x ".git/" "debate/public" "gs://www.ashtonc.ca/debate"
+deploy_static=false
+deploy_app_engine=true
 
-# Blog content
-#cd blog; hugo; cd ..
-#gsutil -m rsync -r -x ".git/" "blog/public" "gs://www.ashtonc.ca/blog"
+# Static Site Bucket
 
+if [ "$deploy_static" = true ]; then
+	echo "Deploying to static bucket."
+
+	echo "Syncing local files with bucket $home_bucket."
+
+	## Base home directory
+	echo "> Root..."
+	gsutil -m rsync -r -x ".git/" "ashtonc" "gs://$home_bucket"
+
+	## TA content
+	echo "> TA..."
+	cd ta; hugo; cd ..
+	gsutil -m rsync -r -x ".git/" "ta/public" "gs://$home_bucket/ta"
+
+	## Debate content
+	echo "> Debate..."
+	cd debate; hugo; cd ..
+	gsutil -m rsync -r -x ".git/" "debate/public" "gs://$home_bucket/debate"
+
+	## Blog content
+	#cd blog; hugo; cd ..
+	#gsutil -m rsync -r -x ".git/" "blog/public" "gs://$home_bucket/blog"
+
+fi
+
+# App Engine
+
+if [ "$deploy_app_engine" = true ]; then
+	echo "Deploying to app engine."
+
+	gcloud config set project ashtonc-home
+
+	rsync -r home/ $home_app_engine/static
+	#rsync /ta/public /$home_app_engine/static/ta
+
+	cd $home_app_engine
+	gcloud app deploy -v 1
+	cd ..
+fi
