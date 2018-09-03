591d0cdc8d72b434b9e84a46625cfb51c0d5ed83
Ashton Charbonneau <ashton@ashtonc.ca>
2018-09-02 23:35:51 -0600

    Reconfigure kubernetes deployment

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ kubernetes_dir="kubernetes-ashtonc-home"
 
 deploy_static=false
 deploy_app_engine=false
-deploy_kubernetes=true
+deploy_kubernetes=false
 
 # Cloud Storage Bucket
 
@@ -53,11 +53,38 @@ fi
 if [ "$deploy_kubernetes" = true ]; then
 	echo "Deploying to Kubernetes..."
 
+	# 1. Place your static files inside a bucket
 	gsutil -m rsync -d "$kubernetes_dir/nginx" "gs://$test_bucket/deploy"
 	gsutil -m rsync -d -r -x ".git/" "home" "gs://$test_bucket/static"
 
-	cd $kubernetes_dir
-		kubectl apply -f k8s.yaml
-	cd ..
+	# 2. Create a cluster (online for now)
+
+	# 3. Initialize the cluster locally
+	gcloud container clusters get-credentials ashtonc-home
+
+	# 4. Install Helm on your cluster
+	kubectl create serviceaccount --namespace kube-system tiller
+	kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+	kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'      
+	helm init --service-account tiller --upgrade
+
+	# 5. Use Helm to install nginx-ingress
+	helm install stable/nginx-ingress --name nginx-ingress --namespace kube-system --set rbac.create=true #--set controller.hostNetwork=true,controller.kind=DaemonSet
+
+	# 5.1 Configure DNS to point to the nginx ingress load balancer
+
+	# 6. Use Helm to install cert-manager
+	helm install stable/cert-manager --name cert-manager --namespace kube-system --set ingressShim.defaultIssuerName=letsencrypt-staging --set ingressShim.defaultIssuerKind=ClusterIssuer
+	kubectl apply -f $kubernetes_dir/cert-manager/letsencrypt-staging.yaml
+
+	# 7. Initialize your deployment
+	kubectl apply -f $kubernetes_dir/k8s/deployment.yaml
+
+	# 8. Initialize your service
+	kubectl apply -f $kubernetes_dir/k8s/service.yaml
+
+	# 9. Initialize your ingress
+	kubectl apply -f $kubernetes_dir/k8s/ingress.yaml
+
 fi
 
